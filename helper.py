f24111286ef1806b0b353cd522a5a29fff8c1da2
Puneet15556 <puneet.22jgm@gmail.com>
2025-08-13 19:11:36 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@
 
 import os
 from langchain_groq import ChatGroq
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "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"
 
 
 
